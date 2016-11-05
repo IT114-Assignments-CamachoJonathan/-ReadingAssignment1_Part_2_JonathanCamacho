2690789f3f7765c7fc3441a1b6996640c8073d4e
Jonathan Camacho <Jonathan Camacho@DESKTOP-4V427KT.fios-router.home>
2016-11-05 11:29:09 -0400

    Initial commit

---
+++ src/readingAss1_98_java_JonathanCamacho/Fan.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package readingAss1_98_java_JonathanCamacho;
+
+/**
+ * @author Jonathan Camacho
+ *
+ */
+public class Fan {
+	int SLOW=1;
+	
+	int MEDIUM=2;
+	
+	int FAST=3;
+	
+	private int speed() {
+		return SLOW;
+		
+	}
+
+	private boolean on() {
+		return false;
+		
+	}
+	
+	private double radius() {
+		return 5;
+	}
+	
+	public String color() {
+		return blue;
+	}
+	
+	default.Perform(speed,on,radius,color) {
+		
+	}
+	
+	new fan(default) {
+		
+	}
+	
+	public String toString() {
+		return 
+	}
+}
